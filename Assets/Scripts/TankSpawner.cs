333317319b243f1cb7418e27ecb802b2f77cae51
DESKTOP-6U5DPEC\koush <koushikavasudevan@gmail.com>
2024-07-30 01:49:33 +0530

    Implemented different types of tanks.

--- Assets/Scripts/TankSpawner.cs
+++ Assets/Scripts/TankSpawner.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class TankSpawner : MonoBehaviour
 {
+    [System.Serializable]
+    public class Tank
+    {
+        public float movementSpeed;
+        public float rotationSpeed;
+
+        public TankTypes tankType;
+
+        public Material color;
+    }
+
+    public List<Tank> tankList;
+
     public TankView TankView;
 
     // Start is called before the first frame update
@@ -14,7 +27,7 @@ public class TankSpawner : MonoBehaviour
 
     private void CreateTank()
     {
-        TankModel tankModel = new TankModel(30, 20);
+        TankModel tankModel = new TankModel(tankList[0].movementSpeed, tankList[0].rotationSpeed, tankList[0].tankType, tankList[0].color);
         TankController tankController = new TankController(tankModel, TankView);
     }
 }
--- Assets/Scripts/TankView.cs
+++ Assets/Scripts/TankView.cs
@@ -10,6 +10,8 @@ public class TankView : MonoBehaviour
     private float rotation;
 
     public Rigidbody rb;
+
+    public MeshRenderer[] childs;
     // Start is called before the first frame update
     void Start()
     {
@@ -48,5 +50,13 @@ public class TankView : MonoBehaviour
     public Rigidbody GetRigidbody()
     {
         return rb;
+    }
+
+    public void ChangeColor(Material color)
+    {
+        for(int i = 0; i < childs.Length; i++)
+        {
+            childs[i].material = color;
+        }
     }
 }
